a5c2303690a85a1af43a9835a20a3d724be457ac
mhstudio666 <156029214+mhstudio666@users.noreply.github.com>
2024-06-06 09:15:03 -0500

    Add files via upload

---
+++ hubspot-form-raw.js
@@ -0,0 +1,105 @@
+.hs-input, .legal-consent-container label:not(.hs-error-msg), .hs-form-field label:not(.hs-error-msg), .legend.hs-field-desc, .hs-button, .legal-consent-container .hs-form-booleancheckbox-display p {
+    font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif;
+}
+.hs-input, .hs-form .field, .legal-consent-container label:not(.hs-error-msg), .hs-error-msgs label, .select.is-placeholder, .legend.hs-field-desc {
+    color: #33475b;
+}
+.hs-input {
+    display: inline-block;
+    width: 90%;
+    max-width: 500px;
+    height: 40px;
+    padding: 9px 20px;
+    font-size: 16px;
+    line-height: 22px;
+    border: 1px solid #cbd6e2;
+    box-sizing: border-box;
+    border-radius: 3px;
+    background-color: #f5f8fa;
+}
+legend.hs-field-desc {
+    font-size: 11px;
+}
+label {
+    margin-bottom: 5px;
+    font-weight: 700;
+    display: block;
+}
+.hs-custom-style .hs-input, .hs-custom-style fieldset {
+    max-width: 100%;
+}
+.hs-form .field {
+    margin-bottom: 18px;
+    font-weight: normal;
+}
+.hs-form-field input[type=text], .hs-form-field input[type=email], .hs-form-field input[type=phone], .hs-form-field input[type=number], .hs-form-field input[type=tel], .hs-form-field input[type=date], .hs-form-field select, .hs-form-field textarea {
+    border-radius: 15px;
+    background-clip: padding-box;
+    padding: 10px 15px;
+}
+.hs-form-required {
+    color: red;
+}
+.hs-input option:disabled {
+    color: #bfbfbf;
+}
+select.is-placeholder {
+    color: #666;
+}
+.hs-form li {
+    display: block;
+    text-align: -webkit-match-parent;
+}
+.hs-error-msgs label {
+    color: #f2545b;
+}
+.hs-button {
+    border-radius: 15px;
+    background-clip: padding-box;
+    font-size: 14px;
+    line-height: 12px;
+    margin: 20px 0 0;
+    cursor: pointer;
+    display: inline-block;
+    font-weight: 700;
+    position: relative;
+    text-align: center;
+    transition: all .15s linear;
+    background-color: #921555;
+    border-color: #921555;
+    color: #fff;
+    border: 1px solid;
+    padding: 12px 24px;
+}
+.hs-input[type=checkbox], .hs-input[type=radio] {
+    cursor: pointer;
+    width: auto;
+    height: auto;
+    padding: 0;
+    margin: 3px 5px 3px 0;
+    line-height: normal;
+    border: none;
+}
+.legal-consent-container label:not(.hs-error-msg) {
+    font-size: 14px;
+}
+.inputs-list {
+    margin: 0 0 5px;
+    width: 100%;
+    padding-left: 5px;
+}
+.hs-form-ad984611-f980-441e-8ced-a97343fba8e9_15f5658f-1251-41ff-a92c-9998762cc631 fieldset {
+    border: 0;
+    padding: 0;
+    margin: 0 auto;
+    width: 500px;
+}
+.legal-consent-container .hs-form-booleancheckbox-display p {
+    margin: 20px 0 0;
+    display: inline;
+    font-size: 12px;
+    line-height: 110%;
+}
+.w-richtext figure.w-richtext-align-center.w-richtext-figure-type-image>div, .w-richtext figure.w-richtext-align-center[data-rt-type=image]>div {
+    width: 100%;
+}
